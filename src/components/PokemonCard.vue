29cbe284002595ff89bb945071f20f554310115a
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-09 15:58:29 +0300

    some css

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { getImageUrl } from '@/utils/utils'
-import _ from 'lodash'
+import { ref, onMounted } from "vue";
+import { getImageUrl } from "@/utils/utils";
+import _ from "lodash";
 
 const { name, url } = defineProps({
   name: String,
-  url: String
-})
+  url: String,
+});
 
-const imageUrl = ref('')
-const detailsUrl = ref('')
+const imageUrl = ref("");
+const detailsUrl = ref("");
 
 onMounted(() => {
-  const pokemonIndex = parseInt((url as string)?.split('/')?.[(url as string).split('/').length - 2] ?? '0')
-  detailsUrl.value = `/pokemon/${pokemonIndex}`
-  imageUrl.value = getImageUrl(pokemonIndex)
-})
+  const pokemonIndex = parseInt(
+    (url as string)?.split("/")?.[(url as string).split("/").length - 2] ?? "0"
+  );
+  detailsUrl.value = `/pokemon/${pokemonIndex}`;
+  imageUrl.value = getImageUrl(pokemonIndex);
+});
 </script>
 
 <template lang="pug">
 
-RouterLink(:to="detailsUrl" class="bg-gray-200 p-5 rounded-lg flex flex-col items-center cursor-pointer hover:bg-gray-300 transition duration-300 ease-in-out")
+RouterLink(:to="detailsUrl" class="bg-white bg-opacity-25 p-5 rounded-lg flex flex-col shadow-2xl items-center cursor-pointer hover:bg-gray-300 transition duration-300 ease-in-out")
   img(:src="imageUrl" alt="Vue logo" width="70" height="50")
   p(class="text-black") {{ _.capitalize(name) }}
 
--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { getPokemon } from '@/service/service'
-import { ref, onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import _ from 'lodash'
-import { getImageUrl } from '@/utils/utils'
+import { useRoute } from "vue-router";
+import { getPokemon } from "@/service/service";
+import { ref, onMounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import _ from "lodash";
+import { getImageUrl } from "@/utils/utils";
 
-const idParam = useRoute().params.id
-const imageUrl = getImageUrl(parseInt(idParam as string))
-const pokemon = ref(null)
+const idParam = useRoute().params.id;
+const imageUrl = getImageUrl(parseInt(idParam as string));
+const pokemon = ref(null);
 
 onMounted(async () => {
-  pokemon.value = await getPokemon(idParam as string)
-})
+  pokemon.value = await getPokemon(idParam as string);
+});
 </script>
 
 <template lang="pug">
 
 
 div(class="flex flex-col justify-center items-center p-10 h-[calc(100vh-130px)]")
-  div(class="bg-gray-200 p-5 w-full md:w-[400px] h-min-[500px] rounded-lg flex flex-col items-center hover:bg-gray-300 transition duration-300 ease-in-out")
+  div(class="bg-white bg-opacity-25 p-5 w-full md:w-[400px] h-min-[500px] rounded-lg flex flex-col items-center hover:bg-gray-300 transition duration-300 ease-in-out")
     div(class="flex flex-col items-center")
       div(class="flex justify-between w-full")
         img(:src="imageUrl" alt="Vue logo" width="200" height="50" class="m-auto")
