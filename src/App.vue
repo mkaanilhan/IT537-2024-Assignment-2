6e65f80cebaa308ae77ceabadf2be1dd14d1696e
Eray Ermiş <eray.ermis@sabanciuniv.edu>
2024-07-21 19:42:16 +0300

    Header ve Footer created

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+
+
+</script>
+
+<template>
+
+
+<div class="flex flex-col justify-between items-center min-h-[100vh]">
+
+  <header class="bg-blue-300 w-full">
+    POKEMON
+  </header>
+
+
+  <footer class="bg-blue-300 w-full text-center">
+
+    <div>
+      Pokemon_App
+    </div>
+
+    <div>
+      by Eray Ermis
+
+    </div>
+
+  </footer>
+
+</div>
+
+  
+
+
+
+</template>
